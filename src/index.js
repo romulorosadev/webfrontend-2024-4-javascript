12e71074ea44201360b64e9f5217e6873881080b
Romulo Rosa <romulorosadev@gmail.com>
2024-09-24 20:31:14 -0300

    const numeroMesa function validarMersa

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 // Importando a biblioteca readline-sync
 const readline = require('readline-sync');
 
-// Capturando o número de pessoas na mesa
-const numeroPessoas = readline.questionInt('Digite o número de pessoas na mesa: ');
+// Capturando o numero da mesa
+const numeroMesa = readline.questionInt('Digite o número da mesa: ');
 
 // Capturando o valor total da conta
 const valorTotal = readline.questionFloat('Digite o valor total da conta: ');
+
+// Capturando o número de pessoas na mesa
+const numeroPessoas = readline.questionInt('Digite o número de pessoas na mesa: ');
 
 // Capturando o método de pagamento
 const metodoPagamento = readline.question('Qual é o método de pagamento (PIX, dinheiro ou cartão)? ');
 
 
-//Façã sua lógica para aplicar o desconto apenas para PIX OU DINHEIRO
+//Faça sua lógica para aplicar o desconto apenas para PIX OU DINHEIRO
+
+function validarMesa(numeroMesa) {
+  const mesaValida = (numeroMesa > 0 && numeroMesa < 31) ? true : false;
+  return mesaValida
+}
+
 
 
 // Exibindo os resultados
+console.log(validarMesa(numeroMesa));
