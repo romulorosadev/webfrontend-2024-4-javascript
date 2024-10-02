a18b3f21dc8ebe04d6c31ab7868eb14df7c3db66
Romulo Rosa <romulorosadev@gmail.com>
2024-10-02 01:14:28 -0300

    jest criacao jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  testEnvironment: 'node',  // Define o ambiente de testes como Node.js
+  testMatch: ['**/__tests__/**/*.test.js'],  // Define onde os arquivos de teste estarão localizados
+  verbose: true  // Para exibir mais detalhes na saída dos testes
+};
